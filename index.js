3ca97dbbfd5d7666ab8ae09be70d7e39344d24f6
Noodle96 <jorgealfredomarino2@gmail.com>
2024-04-01 17:48:16 -0500

    Connect mongoose to our cluster and run the event on with connected and error.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -5,11 +6,26 @@ const app = express();
 //Accept request body from the client
 app.use(express.json());
 
+//CONNECT MONGOOSE TO OUR CLUSTER
+mongoose.connect(process.env.MONGO_URI);
+
+//Event to check if we are connected to the database
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose is connected');
+	// console.log(process.env.MONGO_URI);
+	app.listen(3000, () => { console.log('Server is running on port 3000');});
+});
+//Event to check if we are disconnected to the database
+mongoose.connection.on('error', (err) => {
+	console.log('Mongoose connection error:', err);
+});
+
+//Collection: Table
+//Document: Row
+
 //Request: What fronted sends us.
 // Response: What we send back to the Clientt.
 app.post('/', (req, res) => {
 	console.log(req.body);
 	res.send('Hello World since index.js');
 });
-
-app.listen(3000, () => { console.log('Server is running on port 3000');});
